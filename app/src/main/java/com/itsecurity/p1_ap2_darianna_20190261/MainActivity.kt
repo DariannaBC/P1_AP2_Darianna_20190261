98f5e15793a37550528690862e03910b72282e9a
Darianna Cruz De La Rosa <darianna_cruz@ucne.edu.do>
2022-06-08 19:32:02 -0400

    Quitando cosas innecesarias del main

--- app/src/main/java/com/itsecurity/p1_ap2_darianna_20190261/MainActivity.kt
+++ app/src/main/java/com/itsecurity/p1_ap2_darianna_20190261/MainActivity.kt
@@ -11,23 +11,19 @@ import androidx.activity.compose.setContent
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
-import com.itsecurity.p1_ap2_darianna_20190261.ui.theme.Parcial1Ap2.ConsultaParcial1Ap2Screen
-import com.itsecurity.p1_ap2_darianna_20190261.ui.theme.Parcial1Ap2.RegistroParcial1Ap2Screen
+import com.itsecurity.p1_ap2_darianna_20190261.ui.componentes.Prestamo.ConsultaPrestamosScreen
+import com.itsecurity.p1_ap2_darianna_20190261.ui.componentes.Prestamo.RegistroPrestamoScreen
+import com.itsecurity.p1_ap2_darianna_20190261.ui.theme.RegistroPrestamosComposeTheme
 
-
-import com.itsecurity.p1_ap2_darianna_20190261.ui.theme.RegistroParcial1Ap2ComposeTheme
 import dagger.hilt.android.AndroidEntryPoint
 
-
-
-var selectedOcupacion: String? = null
 
 @AndroidEntryPoint
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            RegistroParcial1Ap2ComposeTheme() {
+            RegistroPrestamosComposeTheme() {
                 // A surface container using the 'background' color from the theme
                 Surface(
                     modifier = Modifier.fillMaxSize(),
@@ -40,46 +36,32 @@ class MainActivity : ComponentActivity() {
     }
 }
 
-//TODO Implementar navegacion correspondiente
+
 @Composable
 fun MyApp() {
     val navHostController = rememberNavController()
 
-    NavHost(navController = navHostController, startDestination = "ConsultaParcial1Ap2"){
-        composable("ConsultaParcial1Ap2"){
-            ConsultaParcial1Ap2Screen(navHostController = navHostController)
+    NavHost(navController = navHostController, startDestination = "ConsultaPrestamos"){
+        composable("ConsultaPrestamos"){
+            ConsultaPrestamosScreen(navHostController = navHostController)
         }
-        composable("RegistroParcial1Ap2"){
-            RegistroParcial1Ap2Screen(navHostController = navHostController)
+        composable("RegistroPrestamos"){
+            RegistroPrestamoScreen(navHostController = navHostController)
         }
     }
 }
 
-
-
-
-
-
-
-
 @Composable
-fun RowPersona(nombre:String){
+fun RowPrestamos(nombre:String){
     Row() {
         Text(text = "El nombre es: $nombre")
-    }
-}
-
-@Composable
-fun RowOcupacion(ocupacion:String){
-    Row() {
-        Text(text = "El nombre de la Ocupación es: $ocupacion")
     }
 }
 
 @Preview(showBackground = true, showSystemUi = true)
 @Composable
 fun DefaultPreview() {
-    RegistroParcial1Ap2ComposeTheme() {
+    RegistroPrestamosComposeTheme() {
         MyApp()
     }
 }
